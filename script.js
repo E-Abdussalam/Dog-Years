fd6d220e68b6b6863ce9c64c18ced0bc43ba30e9
Emhemmed Abdussalam <emhemmed@Emhemmeds-iMac.local>
2021-12-13 21:44:44 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const myAge = 34;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let = lateYears = myAge - 2;
+lateYears *= 4;
+console.log(earlyYears);
+console.log(lateYears);
+const myAgeInDogYears = earlyYears + lateYears;
+const myName = "Emhemmed Abdussalam".toLowerCase();
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
